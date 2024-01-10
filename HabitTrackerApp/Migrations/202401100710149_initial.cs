2890a3f2066078c0fa5be0f92f7406a4f53d6fe5
sanahaju123 <sanahaju123@gmail.com>
2024-01-10 13:00:51 +0530

    updated

--- HabitTrackerApp/Migrations/202401100710149_initial.cs
+++ HabitTrackerApp/Migrations/202401100710149_initial.cs
@@ -1,9 +1,9 @@
-namespace HabitTrackerApp.Migrations
+﻿namespace FitnessTrackerApp.Migrations
 {
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class InitialCreate : DbMigration
+    public partial class initial : DbMigration
     {
         public override void Up()
         {
@@ -12,10 +12,10 @@ namespace HabitTrackerApp.Migrations
                 c => new
                     {
                         Id = c.Int(nullable: false, identity: true),
-                        Exercise = c.String(),
-                        Sets = c.Int(nullable: false),
-                        Reps = c.Int(nullable: false),
                         Date = c.DateTime(nullable: false),
+                        HabitName = c.String(),
+                        IsCompleted = c.Boolean(nullable: false),
+                        Notes = c.String(),
                     })
                 .PrimaryKey(t => t.Id);
             
